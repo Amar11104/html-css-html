993a4144b74bd4f4386002b754e6cf73be2b635e
Amar11104 <amar.dushulovci@gmail.com>
2025-03-26 12:43:55 -0700

    moduli26

---
+++ moduli26/main.js
@@ -0,0 +1,29 @@
+console.log("Hellooo");
+
+var a = 50 +50;
+console.log(a);
+
+var b = 20;
+var c = 30;
+var d = b+c;
+console.log(d);
+
+var e = 20;
+var f = 10;
+var g = e-f;
+console.log(d);
+
+var h = 50;
+var i = 10;
+var rezultati = h/i;
+console.log(rezultati);
+
+var n=10;
+n++;
+console.log(n);
+
+
+var o = 100;
+o--;
+console.log(o);
+
