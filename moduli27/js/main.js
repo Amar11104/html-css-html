a5f6527428bec489aae2836374fb4acbe7aced13
Amar11104 <amar.dushulovci@gmail.com>
2025-04-02 11:47:28 -0700

    moduli 27

---
+++ moduli27/js/main.js
@@ -0,0 +1,40 @@
+// function showMessage() {
+//     alert("Hello");
+
+// }
+
+function sum(num1,num2) {
+    return num1 + num2;
+}
+
+function dsFunction() {
+    var localVar = "Shkolla Digjitale";
+    alert(localVar);
+
+}
+dsFunction();
+
+function toSeconds(minutes) {
+    return minutes * 60;
+}
+
+console.log(toSeconds(60))
+
+function siperfaqjaeTrekendshit(a,h) {
+    return 0.5 * a * h;
+}
+
+console.log(siperfaqjaeTrekendshit(1,6));
+
+var vetura = {
+    marka: "BMW"
+    color: "grey"
+    km: "00"
+    year: "2025"
+}
+
+alert(vetura.marka);
+
+console.log(sum(25,5));
+
+// showMessage();
