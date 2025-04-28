5128559e9b035358e063fa4b045cd3afc0b483b0
Amar11104 <amar.dushulovci@gmail.com>
2025-04-28 11:47:56 -0700

    moduli30

---
+++ moduli30/main.js
@@ -0,0 +1,28 @@
+var x = 1;
+
+if(x > 2) {
+    console.log("x eshte me i madh se vlera 2")
+}
+
+else {
+    console.log("x eshte me i madh se vlera 2")
+}
+
+
+var input = document.getElementById('input_id');
+var button = document.getElementById('btn_id');
+var text = document.getElementById('text_id');
+
+button.onclick = function(){
+    text.this.innerHTML = input.value;
+}
+
+var input1 = document.querySelector('#input1_id');
+var input2 = document.querySelector('#input2_id');
+var btn2 = document.querySelector('#btn2_id');
+var answer = document.querySelector('#answer');
+
+
+btn2.addEventListener("click", function(){
+    answer.innerHTML = parseInt(input1.value) + parseInt(input2.value);
+});
