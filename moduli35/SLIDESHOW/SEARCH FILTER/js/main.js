3a737e712e6390105a95f99388e6388a97ddacdd
Amar11104 <amar.dushulovci@gmail.com>
2025-05-21 11:48:35 -0700

    moduli35

---
+++ moduli35/SLIDESHOW/SEARCH FILTER/js/main.js
@@ -0,0 +1,14 @@
+function search() {
+    var input, filter, ul, li, a, i, txtValue;
+
+}
+
+input = document.getElementById("myInput");
+
+filter = input.value.toUpperCase();
+
+ul = document.getElementById("myList");
+
+for (i = 0; i<li.length; i++) {
+    a=li[i];
+}
