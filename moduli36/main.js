f3a666ab113314b46f35263b80325c88c26cfc58
Amar11104 <amar.dushulovci@gmail.com>
2025-05-28 11:11:26 -0700

    moduli36

--- moduli36/main.js
+++ moduli36/main.js
@@ -14,8 +14,23 @@ $('li').click(function(){
 	alert('clicked');
 });
 
-$('#btn').click(function() {
-    console.log ($('#h1') text());
-    $('#h1').text('Testing text');
-    $('#h1').append('Extra text');
+// $('#btn').click(function() {
+//     console.log ($('#h1') text());
+//     $('#h1').text('Testing text');
+//     $('#h1').append('Extra text');
+// });
+
+$("#btn3").click(function(){
+    $(".hidden").show('slow');
+});
+
+$("#btn4").click(function(){
+    $(".hidden").hide();
+});
+
+$('#square').click(function(){
+    $('#square').animate({
+        'width': '200px',
+        'height': '200px'
+    });
 });
